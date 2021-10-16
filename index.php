a76105bcb3b5d2afb939a6da60c165f9d86a9d61
ahishakiyeyv <yvanahishakiye6@gmail.com>
2021-10-16 23:40:48 +0200

    first commit index

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/bb.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://kit.fontawesome.com/7e089e6dcc.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Portfolio|BUDIGO Bertrand</title>
+</head>
+
+<body>
+    <aside>
+        <div class="search">
+            <form action="" method="post">
+                <input type="search" name="recherche" id="searc" placeholder="votre Recherche...">
+                <button type="submit" id="btn"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+        <div class="tweet">
+            <a class="twitter-timeline" href="https://twitter.com/BBudigo?ref_src=twsrc%5Etfw">Tweets by BBudigo</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+    </aside>
+    <article>
+        <section class="container1">
+            <div class="content1">
+                <div class="div1">
+                    <p class="port">Portfolio</p>
+                </div>
+                <div class="menu">
+                    <ul class="ul">
+                        <li class="li"><a class="menulink" href="#container3">Présentation</a> </li>
+                        <li class="li"><a class="menulink" href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="content2">
+                <div class="presentation">
+                    <div class="div2">
+                        <p class="pre">Je me présente,<br>BUDIGO Bertrand.</p>
+                        <p class="pre1">La lecture,Mon unique passion.</p>
+                    </div>
+                    <div class="plus">
+                        <!-- Button trigger modal -->
+<button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  En savoir plus
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">La lecture</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>la lecture pour moi est une passion, j'ai été inspiré par cet adage que Victor HUGO a dit:<strong>"Lire, c'est boire et manger. L'esprit qui ne lit pas maigrit comme le corps qui ne mange pas"</strong></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+
+      </div>
+    </div>
+  </div>
+</div>
+                    </div>
+                </div>
+                <div class="image1">
+                    <img class="image" src="images/01.jpg" alt="Image Non Telecharger">
+                </div>
+            </div>
+        </section>
+        <section class="container2">
+            <div class="identite">
+                <h2 class="h2">Qui Suis-je?</h2>
+                <hr style="width:150px;">
+                <p class="parag">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et at fugiat perferendis
+                    alias.
+                    Fugiat, eos!
+                    Alias, aperiam? Necessitatibus quia, id blanditiis, qui ratione corrupti esse eaque quam similique,
+                    nulla dignissimos!</p>
+                <a class="more" href="#">Aller Voir</a>
+            </div>
+            <div class="competence">
+                <h2 class="h2">Compétences</h2>
+                <hr style="width:150px;">
+                <p class="parag">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis reiciendis facilis
+                    voluptatibus nostrum
+                    laboriosam odit possimus est quos officia quae. Magni adipisci architecto fugit sapiente nemo,
+                    ducimus
+                    maxime ea repudiandae.</p>
+                <a class="more" href="#">Aller Voir</a>
+            </div>
+            <div class="realisation">
+                <h2 class="h2">Realisation</h2>
+                <hr style="width:150px;">
+                <p class="parag">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia consectetur deserunt,
+                    vero accusantium
+                    numquam quis laboriosam pariatur expedita, corporis earum explicabo amet incidunt? Quas velit
+                    facilis
+                    ducimus aspernatur ea vero!</p>
+                <a class="more" href="#">Aller Voir</a>
+            </div>
+        </section>
+        <section id="container3">
+            <div class="div3">
+                <h3>Qui suis-je?</h3>
+                <p class="para">Je me présente, BUDIGO Bertrand 24ans étudiant à l'Université Espoir d'Afrique, Faculté
+                    des affaires et sciences professionnels, Département de Droit.</p>
+            </div>
+            <div class="trois">
+                <div class="un">
+                    <img class="imag" src="images/hame.jpg" alt="image non telecharger">
+                </div>
+                <div class="deux">
+                    <img class="imag" src="images/justice.jpg" alt="image non telecharger">
+                </div>
+                <div class="three">
+                    <img class="imag" src="images/ham.jpg" alt="image non telecharger">
+                </div>
+            </div>
+        </section>
+        <section class="container4">
+            <h1 class="title">Mes realisations</h1>
+            <div class="content4">
+
+                <a class="box1" href="#">Apple</a>
+
+
+                <a class="box2" href="#">Projet</a>
+
+            </div>
+        </section>
+    </article>
+    <footer>
+        <div class="copy">
+            <p>&copy 2021 BUDIGO Bertrand, Tous Droits Réservés</p>
+        </div>
+        <div class="contact">
+            <p class="cont">Contact: <a class="media" href="facebook.com"><i class="fab fa-facebook-square"></i></a> <a class="media" href="twitter.com"><i class="fab fa-twitter-square"></i></a> <a
+                    class="media" href="mailto:bertrandbudigohau@gmail.com" target:_blank><i class="fas fa-envelope-square"></i></a></p>
+        </div>
+        <p class="dev">Developped by Y.I Ahishakiye</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+
+</html>
